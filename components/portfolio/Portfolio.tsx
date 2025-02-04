16b87eded40f6c273e4def66d677232fc4aedd4f
Muhammad-Bilal <muhammadbilal0729@gmail.com>
2025-02-04 23:11:59 +0500

    fix:"responsiveness issue"

--- components/portfolio/Portfolio.tsx
+++ components/portfolio/Portfolio.tsx
@@ -5,43 +5,60 @@ import twoPerson from "@/public/assets/images/twoPerson.svg";
 
 const NetWorthTracker = () => {
   return (
-    <div>
+    <div className="max-md:hidden">
       <div
-        className="flex max-sm:flex-col justify-center items-center text-white p-4 sm:p-6 shadow-lg w-full"
+        className="flex justify-center items-center  text-white p-6 shadow-lg w-full"
         style={{
-          backgroundImage: "url('assets/images/cyanBackground.svg')",
+          backgroundImage: "url('/Background (Icons).png')",
           backgroundSize: "contain",
           backgroundPosition: "center",
           backgroundColor: "#00abe1",
         }}
       >
-        <div className="rounded-lg max-sm:w-64 max-sm:h-72 sm:w-96 sm:h-100 border-8 sm:border-20 border-white">
+        <div
+          style={{
+            width: "384px",
+            height: "400px",
+            borderRadius: "34px",
+            border: "20px solid",
+          }}
+          className="rounded-lg"
+        >
           <div
-            className="p-4 rounded-lg h-full"
+            className="p-4 rounded-lg "
             style={{
               backgroundImage: "url('/Rectangle.jpg')",
-              backgroundSize: "cover",
+              backgroundSize: "auto",
               backgroundPosition: "center",
             }}
           >
-            <Image className="mt-5" alt="bar code" src={barCode} />
+            <Image className="mt-5 w-full " src={barCode} alt="change" />
           </div>
         </div>
 
-        <div className="flex items-center sm:ml-4 text-left max-sm:mt-4">
-          <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-5 bg-white bg-opacity-90 rounded-lg max-sm:w-72 sm:w-[730px]">
+        <div className="flex items-center ml-2 text-left">
+          {" "}
+          <div
+            style={{ width: "730px" }}
+            className="ml-2 flex gap-5 bg-white bg-opacity-90 p-4 rounded-lg"
+          >
+            {" "}
             <div>
               <Image
                 src={twoPerson}
                 alt="Illustration of two people talking"
-                className="max-sm:w-40 lg:h-[360px]"
+                style={{ width: "258px", position: "relative", right: "83px" }}
               />
             </div>
-            <div className="flex items-center justify-center">
-              <p className="text-sm sm:text-lg font-bold text-cyan leading-5 sm:leading-6 text-center sm:text-left p-3">
-                Sign up today and manage your portfolio like a pro with a
-                personalized assistant and access to a trusted tech community of
-                1,200 members who connected $150M+ of total assets.
+            <div className="flex items-center justify-center ">
+              <p
+                style={{ color: "rgba(18, 115, 152, 1)" }}
+                className="text-lg font-bold w-80 leading-6"
+              >
+                <span style={{ color: "rgb(24 102 132)" }}>Sign up today</span>{" "}
+                and manage your portfolio like a pro with a personalized
+                assistant and access to a trusted tech community of 1,200
+                members who connected $150M+ of total assets.
               </p>
             </div>
           </div>
